cfcf5105aaa49c7b6960a3e08cb2c0741b68a448
EthanRin <sovanpanhaarinn@gmail.com>
2024-12-02 23:20:37 +1100

    added git hub deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://splashkit.io/',
+  site: 'https://splashkittest.github.io/',
+  base: 'splashkit.io-starlight',
   // base: '/splashkit.io-starlight',  // if hosted without domain.
   //   output: "server",
   //   adapter: netlify(),
