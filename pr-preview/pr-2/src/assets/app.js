8c0377ea687497771a6645a26050e896c22f876e
EthanRin <EthanRin@users.noreply.github.com>
2024-12-05 10:18:26 +0000

    Deploy preview for PR 2 🛫

--- pr-preview/pr-2/src/assets/app.js
+++
@@ -1,8 +0,0 @@
-// const element = document.getElementById("copy");
-//     element.addEventListener('click', () => {
-//       alert('Button was clicked!');
-//       console.log("hihihi");
-//     });
-export function abc() {
-    console.log("hihihi");
-}
